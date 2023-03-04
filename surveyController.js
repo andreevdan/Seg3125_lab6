dc2f651be0f9907b7f6cb96653f916813bd02ad7
Daniel Andreev <andreevdan@gmail.com>
2023-03-04 18:26:17 -0500

    Update surveyController.js

--- surveyController.js
+++ surveyController.js
@@ -57,7 +57,7 @@ module.exports = function(app){
     // when a user goes to localhost:3000/niceSurvey
     // serve a static html (the survey itself to fill in)
     app.get('/niceSurvey', function(req, res){
-        res.sendFile(__dirname+'/views/niceSurvey.html');
+        res.sendFile(__dirname+'../views/niceSurvey.html');
     });
 
     // when a user types SUBMIT in localhost:3000/niceSurvey 
@@ -80,6 +80,6 @@ module.exports = function(app){
         }
         // mystery line... (if I take it out, the SUBMIT button does change)
         // if anyone can figure this out, let me know!
-        res.sendFile(__dirname + "/views/niceSurvey.html");
+        res.sendFile(__dirname + "../views/niceSurvey.html");
     });
 };
