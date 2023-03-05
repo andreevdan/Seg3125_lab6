da7c7446ea662a931213f0f9fbfb8190e6b71fee
Azan Mubashar <71412049+azan-m@users.noreply.github.com>
2023-03-04 19:24:52 -0500

    Fixed styles showing on node server

--- surveyController.js
+++ surveyController.js
@@ -54,8 +54,11 @@ module.exports = function(app){
         console.log([question1, question2, question3, question4, question5, question6, question7, question8]);
     });
 
+    var express = require('express');
     // when a user goes to localhost:3000/niceSurvey
     // serve a static html (the survey itself to fill in)
+    app.use(express.static(__dirname + '/public'));
+
     app.get('/niceSurvey', function(req, res){
         res.sendFile(__dirname+'/views/niceSurvey.html');
  });
